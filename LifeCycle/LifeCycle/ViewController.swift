4305c22a109d77fa91b1078b1ac7dc13d5ce4d05
Jeojoyoung <jeojoyoung@gmail.com>
2022-03-07 10:48:04 +0900

    Life Cycle 실습

---
+++ LifeCycle/LifeCycle/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by 조영익 on 2022/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("뷰가 로드 되었습니다.")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("뷰가 나타날 것입니다.")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("뷰가 나타났습니다.")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("뷰가 사라질 것입니다.")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    
+        print("뷰가 사라졌습니다.")
+    }
+    
+    
+    @IBAction func btnPressed(_ sender: UIButton) {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(identifier: "SecondController") as SecondController
+        
+        self.navigationController?.pushViewController(viewController, animated: true)
+        
+    }
+    
+}
+
